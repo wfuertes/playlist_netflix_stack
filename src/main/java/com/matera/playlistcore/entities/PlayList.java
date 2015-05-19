e754a84b2e5417592a1c7ae1d789da5866ac883b
Willian Batista <wbatista@kenzanmedia.com>
2015-05-19 12:02:29 -0300

    Initial commit

---
+++ src/main/java/com/matera/playlistcore/entities/PlayList.java
@@ -0,0 +1,63 @@
+package com.matera.playlistcore.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayList {
+
+    private Long id;
+    private String name;
+    private List<String> musics;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    /**
+     * @return the musics
+     */
+    public List<String> getMusics() {
+
+        if (musics == null) {
+            musics = new ArrayList<String>();
+        }
+        return musics;
+    }
+
+    /**
+     * @param musics the musics to set
+     */
+    public void setMusics(List<String> musics) {
+
+        this.musics = musics;
+    }
+
+}
